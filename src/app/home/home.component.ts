60b4e6a300cd16f8e1d8df7326740481915ce62e
mamadoundiaye <ndiayelcoft@gmail.com>
2021-07-31 12:01:13 +0000

    adding services to communicate with the bac end nest-JS and adding service in homeCompon/ents

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FlightsService } from './flights.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private flightsService : FlightsService) { }
 
   ngOnInit(): void {
+  }
+
+
+  getFlights(){
+    
   }
 
 }
