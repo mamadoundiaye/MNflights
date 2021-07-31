3c25c85cf1a05eee6467ecc5648ddfae9709c6b0
mamadoundiaye <ndiayelcoft@gmail.com>
2021-07-31 11:13:10 +0000

    MNflights with angular nestjs and postgres

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { XyzComponent } from './xyz/xyz.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    XyzComponent,
+    HomeComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
